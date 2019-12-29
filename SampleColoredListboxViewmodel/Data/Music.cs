4fd015b02fe3261370dda57604944641863493aa
Ronny Bjones <ronny.bjones@hotmail.com>
2019-12-29 17:39:11 +0100

    Set the color of one item in the list box to red. The XAML illustrates the bindings.

---
+++ SampleColoredListboxViewmodel/Data/Music.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleColoredListboxViewmodel.Data
+{
+    public class Music
+    {
+        public string Title { get; set; }
+
+        public bool IsSpecial { get; set; }
+    }
+}
---
+++ SampleColoredListboxViewmodel/Viewmodel/MusicViewmodel.cs
@@ -0,0 +1,36 @@
+﻿using SampleColoredListboxViewmodel.Data;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleColoredListboxViewmodel.Viewmodel
+{
+    public class MusicViewmodel: INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The event that is fired when any child property changes its value
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        public List<Music> DataSet = new List<Music>
+                {
+                    new Music {Title="And the beat goes on", IsSpecial=false},
+                    new Music {Title="Rithm of the night", IsSpecial=false},
+                    new Music {Title="Careless wispers", IsSpecial=true}
+                };
+
+        public ObservableCollection<Music> MyMusic
+        {
+            get 
+            {
+                return new ObservableCollection<Music>(this.DataSet);
+            }
+
+        }
+
+    }
+}
